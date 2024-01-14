5d84bcc89216036e72271ae328efb138ffcfd711
KG <41345727+kg583@users.noreply.github.com>
2024-01-14 01:30:21 -0500

    Create __init__.py

---
+++ q_series/__init__.py
@@ -0,0 +1,23 @@
+PREC = 50
+
+LL.<z> = LaurentSeriesRing(QQ, default_prec=PREC)
+MM.<q> = PowerSeriesRing(LL, default_prec=PREC)
+
+
+def coeffs(f):
+    return dict(zip(f.exponents(), f.coefficients()))
+
+
+def E(k):
+    return eisenstein_series_qexp(k, prec=PREC, K=LL, normalization="constant")
+
+def T(n, k, eps=None):
+    def inner(f, *args, **kwargs):
+        return hecke_operator_on_qexp(f, n, k, eps, prec=PREC, *args, **kwargs)
+    
+    return inner
+
+delta = delta_qexp(prec=PREC, K=LL)
+j = j_invariant_qexp(prec=PREC, K=LL)
+
+J = E(4)^2 * E(6) / delta / (j - z)
