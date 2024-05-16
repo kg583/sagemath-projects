47e2c5faef5319ab743e681829c348b6b5732412
KG <41345727+kg583@users.noreply.github.com>
2024-05-16 09:22:43 -0500

    Update __init__.py

--- q_series/__init__.py
+++ q_series/__init__.py
@@ -2,31 +2,33 @@ from sage.modular.etaproducts import qexp_eta
 
 PREC = 50
 
-LL.<z> = LaurentSeriesRing(ZZ, default_prec=PREC)
-MM.<q> = PowerSeriesRing(LL, default_prec=PREC)
+Z = ZZ
+# or
+Z.<z> = LaurentSeriesRing(ZZ, default_prec=PREC)
+
+Q.<q> = PowerSeriesRing(Z, default_prec=PREC)
 
 
+# Utilities
 def coeffs(f):
     return dict(zip(f.exponents(), f.coefficients()))
 
 
-def E(k, **kwargs):
-    return eisenstein_series_qexp(k, prec=PREC, K=LL, normalization="constant")(**kwargs)
+# Modular forms
+def E(k):
+    return eisenstein_series_qexp(k, prec=PREC, K=Z, normalization="constant")
 
-def eta(**kwargs):
-    return qexp_eta(MM, PREC)(**kwargs)
+def delta():
+    return delta_qexp(prec=PREC, K=Z)
 
-def delta(**kwargs):
-    return delta_qexp(prec=PREC, K=LL)(**kwargs)
+def j():
+    return j_invariant_qexp(prec=PREC, K=Z)
 
-def j(**kwargs):
-    return j_invariant_qexp(prec=PREC, K=LL)(**kwargs)
+def eta():
+    return qexp_eta(Q, PREC)
 
 def T(n, k, eps=None):
     def inner(f, *args, **kwargs):
         return hecke_operator_on_qexp(f, n, k, eps, prec=PREC, *args, **kwargs)
     
     return inner
-
-def G(**kwargs):
-    return (E(4)^2 * E(6) / delta() / (j() - z))(**kwargs)
